8d81561f91ed9d61503047e80964d8736e6618ec
sajas <sajasam97@gmail.com>
2020-11-03 19:21:04 +0530

    widgets feedback results added

--- frontend/src/app/components/layouts/contents/contents.component.ts
+++ frontend/src/app/components/layouts/contents/contents.component.ts
@@ -36,9 +36,58 @@ export class ContentsComponent implements OnInit {
   public getRowsValue(flag) {
     if (flag === null) {
       return this.feedbacks.length;
-    } else {
-      return this.feedbacks.filter(i => (i.ques1 == flag)).length;
     }
   }
 
+  public getQues1Value1() {
+      return this.feedbacks.filter(i => (i.ques1 == 'vgood')).length;
+  }
+  public getQues1Value2() {
+    return this.feedbacks.filter(i => (i.ques1 == 'good')).length;
+  }
+  public getQues1Value3() {
+    return this.feedbacks.filter(i => (i.ques1 == 'fair')).length;
+  }
+  public getQues1Value4() {
+    return this.feedbacks.filter(i => (i.ques1 == 'poor')).length;
+  }
+
+  public getQues2Value1() {
+    return this.feedbacks.filter(i => (i.ques2 == 'vgood')).length;
+}
+public getQues2Value2() {
+  return this.feedbacks.filter(i => (i.ques2 == 'good')).length;
+}
+public getQues2Value3() {
+  return this.feedbacks.filter(i => (i.ques2 == 'fair')).length;
+}
+public getQues2Value4() {
+  return this.feedbacks.filter(i => (i.ques2 == 'poor')).length;
+}
+
+public getQues3Value1() {
+  return this.feedbacks.filter(i => (i.ques3 == 'vgood')).length;
+}
+public getQues3Value2() {
+return this.feedbacks.filter(i => (i.ques3 == 'good')).length;
+}
+public getQues3Value3() {
+return this.feedbacks.filter(i => (i.ques3 == 'fair')).length;
+}
+public getQues3Value4() {
+return this.feedbacks.filter(i => (i.ques3 == 'poor')).length;
+}
+
+public getQues4Value1() {
+  return this.feedbacks.filter(i => (i.ques4 == 'vgood')).length;
+}
+public getQues4Value2() {
+return this.feedbacks.filter(i => (i.ques4 == 'good')).length;
+}
+public getQues4Value3() {
+return this.feedbacks.filter(i => (i.ques4 == 'fair')).length;
+}
+public getQues4Value4() {
+return this.feedbacks.filter(i => (i.ques4 == 'poor')).length;
+}
 }
